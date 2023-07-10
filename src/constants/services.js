4960a58be90b012a047d52094f1337f4e613b71b
Alanones <109990442+Alanones@users.noreply.github.com>
2023-07-10 09:20:29 +0300

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -6,19 +6,19 @@ const services = [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "software development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Our expert team of software developers excels in crafting customized software solutions tailored to meet your unique business requirements. From ideation to implementation, we provide end-to-end development services, ensuring high-quality, scalable, and efficient software solutions that drive your business growth.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "software design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Our design team brings creativity and innovation to every project, ensuring visually stunning and user-centric designs. Whether it's creating a captivating website interface, designing a compelling logo, or developing an engaging user experience (UX), we strive to enhance your brand identity and leave a lasting impression on your audience.`,
   },
   {
     id: 3,
     icon: <SiWritedotas className="service-icon" />,
     title: "content writing",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Compelling and well-crafted content is vital for engaging your audience and driving conversions. Our talented team of content writers delivers high-quality content that is tailored to your brand's voice and style. From website copy and blog posts to social media content and marketing materials, we provide persuasive and informative content that resonates with your target audience.`,
   },
 ];
 
